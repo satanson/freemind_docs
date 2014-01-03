37c75c0f6908c948fa6b6bb3912309c8a4682835
satanson <ranpanf@gmail.com>
2014-01-03 10:21:58 +0800

    [01/03/14 10:21:58] by satanson

---
+++ MPI.mm
@@ -0,0 +1,4 @@
+<map version="1.0.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1388715680393" ID="ID_1058667574" MODIFIED="1388715685109" TEXT="MPI"/>
+</map>
---
+++ excel.mm
@@ -0,0 +1,32 @@
+<map version="1.0.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1388651530018" ID="ID_171261059" MODIFIED="1388651537412" TEXT="excel">
+<node CREATED="1388655178333" FOLDED="true" ID="ID_489625015" MODIFIED="1388679182448" POSITION="right" TEXT="&#x81ea;&#x5b9a;&#x4e49;&#x5feb;&#x901f;&#x8bbf;&#x95ee;&#x5de5;&#x5177;&#x680f;">
+<node CREATED="1388655192332" ID="ID_1963277522" MODIFIED="1388655243714" TEXT="&#x5f88;&#x591a;&#x5de5;&#x5177;&#x6309;&#x94ae;&#x5728;&#x9762;&#x677f;&#x4e0a;&#x627e;&#x4e0d;&#x5230;, &#x53ef;&#x8bbe;&#x5b9a;&quot;&#x81ea;&#x5b9a;&#x4e49;&#x5feb;&#x901f;&#x8bbf;&#x95ee;&#x5de5;&#x5177;&#x680f;&quot;"/>
+</node>
+<node CREATED="1388651873485" FOLDED="true" ID="ID_37032714" MODIFIED="1388679183480" POSITION="right" TEXT="&#x52a0;&#x5bc6;">
+<node CREATED="1388651900714" ID="ID_614932769" MODIFIED="1388651928345" TEXT="&#x5de6;&#x4e0a;&#x89d2;&quot;Office &#x6309;&#x94ae;&quot;&gt;&quot;&#x51c6;&#x5907;&quot;&gt;&quot;&#x52a0;&#x5bc6;&#x6587;&#x6863;&quot;"/>
+</node>
+<node CREATED="1388654045997" ID="ID_148228503" MODIFIED="1388654052193" POSITION="right" TEXT="&#x516c;&#x5f0f;&#x5ba1;&#x6838;"/>
+<node CREATED="1388654241851" FOLDED="true" ID="ID_1429925514" MODIFIED="1388654806971" POSITION="right" TEXT="&#x6807;&#x9898;&#x5c45;&#x4e2d;">
+<node CREATED="1388654253439" ID="ID_873526883" MODIFIED="1388654277151" TEXT="&#x9009;&#x62e9;&#x5355;&#x5143;&#x683c;&gt;&#x5408;&#x5e76;&#x540e;&#x5c45;&#x4e2d;"/>
+</node>
+<node CREATED="1388654807431" FOLDED="true" ID="ID_1127720852" MODIFIED="1388679181532" POSITION="right" TEXT="&#x8bbe;&#x7f6e;&#x8fb9;&#x6846;">
+<node CREATED="1388654811632" ID="ID_536390565" MODIFIED="1388654839971" TEXT="&#x8bbe;&#x7f6e;&#x5355;&#x5143;&#x683c;&#x683c;&#x5f0f;&gt;&#x8fb9;&#x6846;"/>
+</node>
+<node CREATED="1388678294709" FOLDED="true" ID="ID_365586181" MODIFIED="1388679179516" POSITION="right" TEXT="&#x6bcf;&#x9875;&#x91cd;&#x590d;&#x6253;&#x5370;&#x6807;&#x9898;">
+<node CREATED="1388678332109" ID="ID_319122191" MODIFIED="1388678356998" TEXT="&#x9875;&#x9762;&#x8bbe;&#x7f6e;&gt;&#x9875;&#x9762;&#x5e03;&#x5c40;&gt;&#x6253;&#x5370;&#x6807;&#x9898;"/>
+</node>
+<node CREATED="1388678694097" FOLDED="true" ID="ID_166534821" MODIFIED="1388678934326" POSITION="right" TEXT="&#x884c;&#x9ad8;&#x548c;&#x5217;&#x5bbd;">
+<node CREATED="1388678727168" FOLDED="true" ID="ID_1812848393" MODIFIED="1388678820075" TEXT="&#x884c;(&#x5217;)&#x9009;&#x5b9a;">
+<node CREATED="1388678749637" ID="ID_1479694277" MODIFIED="1388678786298" TEXT="&#x4e00;&#x884c; (&#x70b9;&#x51fb;)"/>
+<node CREATED="1388678759173" ID="ID_322207749" MODIFIED="1388678792749" TEXT="&#x591a;&#x884c; (&#x70b9;&#x51fb;&#x62d6;&#x52a8;)"/>
+<node CREATED="1388678762349" ID="ID_885267148" MODIFIED="1388678818543" TEXT="&#x6240;&#x6709;&#x884c; (&#x5de6;&#x4e0a;&#x89d2;)"/>
+</node>
+<node CREATED="1388678832521" ID="ID_1045997323" MODIFIED="1388678850191" TEXT="&#x5f00;&#x59cb;&gt;&#x683c;&#x5f0f;&gt;&#x5355;&#x5143;&#x683c;&#x5927;&#x5c0f;"/>
+</node>
+<node CREATED="1388678935239" FOLDED="true" ID="ID_335645422" MODIFIED="1388679178600" POSITION="right" TEXT="&#x4e2d;&#x6587;&#x5927;&#x5199;&#x6570;&#x5b57;">
+<node CREATED="1388678944612" ID="ID_96503335" MODIFIED="1388678986132" TEXT="&#x5f00;&#x59cb;&gt;&#x683c;&#x5f0f;&gt;&#x5355;&#x5143;&#x683c;&#x8bbe;&#x7f6e;&gt;&#x6570;&#x5b57;&gt;&#x7279;&#x6b8a;"/>
+</node>
+</node>
+</map>
