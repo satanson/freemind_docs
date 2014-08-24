ff703ac1c47af16b270e6228d7ef54e1ba215c84
satanson <ranpanf@gmail.com>
2014-08-24 13:38:23 +0800

    [08/24/14 13:38:23] by satanson

--- nynn_paper.mm
+++ nynn_paper.mm
@@ -2,7 +2,11 @@
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1408499263847" ID="ID_1218745303" MODIFIED="1408499272723" TEXT="nynn_paper">
 <node CREATED="1408499277471" ID="ID_857831332" MODIFIED="1408537509542" POSITION="right" TEXT="ABSTRACT">
-<node CREATED="1408499288165" ID="ID_1192197970" MODIFIED="1408499313439" TEXT="distributed in-memory file system"/>
+<node CREATED="1408499288165" ID="ID_1192197970" MODIFIED="1408695340124" TEXT="scalable distributed in-memory file system">
+<node CREATED="1408695342755" ID="ID_606364349" MODIFIED="1408695368918" TEXT="multi-billion of small files"/>
+<node CREATED="1408695369311" ID="ID_1331358969" MODIFIED="1408695405306" TEXT="high performance of random access"/>
 </node>
+</node>
+<node CREATED="1408695464019" ID="ID_654075944" MODIFIED="1408695485115" POSITION="right" TEXT="&#x793e;&#x4ea4;&#x7f51;&#x7edc;&#x6570;&#x636e;&#x7684;&#x7279;&#x70b9;"/>
 </node>
 </map>
