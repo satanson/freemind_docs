c8497b7b222d440dfe8a17bf2b28d9815a6499d9
satanson <ranpanf@gmail.com>
2014-12-05 02:11:01 +0800

    [12/05/14 02:11:02] by satanson

---
+++ Simula_Smalltalk.mm
@@ -0,0 +1,53 @@
+<map version="1.0.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1417705975504" ID="ID_1187244787" MODIFIED="1417706001368" TEXT="Simula_Smalltalk">
+<node CREATED="1417706003283" ID="ID_810770112" MODIFIED="1417706033784" POSITION="right" TEXT="Concepts in programming lang"/>
+<node CREATED="1417706057880" ID="ID_1877680243" MODIFIED="1417706561344" POSITION="right" TEXT="Simula">
+<node CREATED="1417706123650" ID="ID_1595633856" MODIFIED="1417706158852" TEXT="authors">
+<node CREATED="1417706077449" ID="ID_1373003492" MODIFIED="1417706128007" TEXT="O.J.Dahl"/>
+<node CREATED="1417706108669" ID="ID_1359614362" MODIFIED="1417706602630" TEXT="K.Nygaard">
+<icon BUILTIN="bookmark"/>
+<node CREATED="1417706846676" ID="ID_1016197817" MODIFIED="1417706874943" TEXT="2001 ACM Turing Award"/>
+<node CREATED="1417706879113" ID="ID_416200857" MODIFIED="1417706898686" TEXT="IEEE John Von Neumann Medal"/>
+</node>
+</node>
+<node CREATED="1417706147118" ID="ID_1455447685" MODIFIED="1417706194756" TEXT="Norwegian Computing Center,Oslo"/>
+<node CREATED="1417706130406" ID="ID_1099090677" MODIFIED="1417706208127" TEXT="1960s"/>
+<node CREATED="1417706215597" ID="ID_841021104" MODIFIED="1417706343181" TEXT="first oop lang, extremely influential, impact/inspire Smalltalk,C++"/>
+<node CREATED="1417706282924" FOLDED="true" ID="ID_1188008827" MODIFIED="1417706463299" TEXT="features">
+<node CREATED="1417706351797" ID="ID_1547259282" MODIFIED="1417706367802" TEXT="classes"/>
+<node CREATED="1417706368526" ID="ID_34116335" MODIFIED="1417706372914" TEXT="objects"/>
+<node CREATED="1417706373225" ID="ID_789159699" MODIFIED="1417706381712" TEXT="dynamic lookup"/>
+<node CREATED="1417706381965" ID="ID_1360831447" MODIFIED="1417706384420" TEXT="subtyping"/>
+<node CREATED="1417706384729" ID="ID_223404013" MODIFIED="1417706393778" TEXT="inheritance"/>
+</node>
+<node CREATED="1417706463761" ID="ID_1094285356" MODIFIED="1417706470079" TEXT="Simula67"/>
+<node CREATED="1417706569688" ID="ID_610562930" MODIFIED="1417706588475" TEXT="event-based simulation"/>
+</node>
+<node CREATED="1417706415688" FOLDED="true" ID="ID_138708314" MODIFIED="1417706966803" POSITION="right" TEXT="Smalltalk">
+<node CREATED="1417706482448" ID="ID_1129632186" MODIFIED="1417706487214" TEXT="authors">
+<node CREATED="1417706488332" ID="ID_1121245582" MODIFIED="1417706492125" TEXT="Alan Kay"/>
+</node>
+<node CREATED="1417706396194" ID="ID_983813122" MODIFIED="1417706432600" TEXT="Xerox Palo Alto Research Center (PARC)"/>
+</node>
+<node CREATED="1417706967741" ID="ID_1238688185" MODIFIED="1417706973234" POSITION="right" TEXT="Simula OOP">
+<node CREATED="1417706981573" FOLDED="true" ID="ID_1770589914" MODIFIED="1417707008119" TEXT="general-purpose object-oriented language">
+<node CREATED="1417706975825" ID="ID_1621956183" MODIFIED="1417707001373" TEXT="Apparently, when someone once asserted that Simula was not a general-purpose language, Nygaard&#x2019;s response was that, because Simula had all of the features of Fortran and Algol, and more, what would he need to remove before Simula could be called general purpose?"/>
+</node>
+<node CREATED="1417707010442" ID="ID_1894435652" MODIFIED="1417707134372" TEXT="Algol 60">
+<node CREATED="1417707135557" ID="ID_1994103046" MODIFIED="1417707136557" TEXT="+">
+<node CREATED="1417707158993" ID="ID_939841429" MODIFIED="1417707158993" TEXT="class concepts and reference variables (pointers to objects),"/>
+<node CREATED="1417707158995" MODIFIED="1417707158995" TEXT="pass-by-reference,"/>
+<node CREATED="1417707158996" MODIFIED="1417707158996" TEXT="char, text, and input&#x2013;output features,"/>
+<node CREATED="1417707158997" MODIFIED="1417707158997" TEXT="coroutines, a mechanism for writing concurrent programs."/>
+</node>
+<node CREATED="1417707137593" ID="ID_1934099917" MODIFIED="1417707138379" TEXT="-">
+<node CREATED="1417707230586" ID="ID_1136749127" MODIFIED="1417707237444" TEXT="changed default parameter passing mechanism from pass-by-name to a com-bination of pass-by-value and pass-by-result,"/>
+<node CREATED="1417707230590" ID="ID_1158474335" MODIFIED="1417707230590" TEXT="some initialization requirements on variables,"/>
+<node CREATED="1417707230591" MODIFIED="1417707230591" TEXT="own variables (which are analogous to C static variables),"/>
+<node CREATED="1417707230592" MODIFIED="1417707230592" TEXT="the Algol 60 string type (in favor of a text type)."/>
+</node>
+</node>
+</node>
+</node>
+</map>
