672398b10e41a3e93ed14d2b099ed8923a7f67a5
satanson <ranpanf@gmail.com>
2015-04-20 20:03:06 +0800

    [04/20/15 20:03:06] by satanson

--- MineCraft.mm
+++ MineCraft.mm
@@ -1,4 +1,16 @@
 <map version="1.0.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1429512386762" ID="ID_1474616557" MODIFIED="1429512393566" TEXT="MineCraft"/>
+<node CREATED="1429512386762" ID="ID_1474616557" MODIFIED="1429512393566" TEXT="MineCraft">
+<node CREATED="1429512435703" ID="ID_1192874395" MODIFIED="1429512444469" POSITION="right" TEXT="sandbox indie grame"/>
+<node CREATED="1429512444922" ID="ID_415050313" MODIFIED="1429512463113" POSITION="right" TEXT="3d virtual world">
+<node CREATED="1429512644338" ID="ID_736163197" MODIFIED="1429512655917" TEXT="player in MC">
+<node CREATED="1429512656900" ID="ID_1753805102" MODIFIED="1429512667617" TEXT="avatar named &quot;Steve&quot;"/>
+</node>
+</node>
+<node CREATED="1429512766569" FOLDED="true" ID="ID_184866466" MODIFIED="1429512889873" POSITION="right" TEXT="indie game">
+<node CREATED="1429512771817" ID="ID_1731691656" MODIFIED="1429512789358" TEXT="independent video game">
+<node CREATED="1429512855568" ID="ID_1375853878" MODIFIED="1429512887439" TEXT="Indie games are &#x201c;independent video games&#x201d;, created by individuals or small teams. They  are often developed without any funding or support from a games publisher. As a result  of their independent nature, indie games are often more innovative than other, more  mainstream games. "/>
+</node>
+</node>
+</node>
 </map>
