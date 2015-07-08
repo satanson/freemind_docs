8baeb4bdaed9d349b604c231cde8d19e8ff045c3
satanson <ranpanf@gmail.com>
2015-07-08 21:47:39 +0800

    by satanson

---
+++ macroservice.mm
@@ -0,0 +1,16 @@
+<map version="1.0.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1433398723017" ID="ID_559001029" MODIFIED="1433398732589" TEXT="macroservice">
+<node CREATED="1433398763299" FOLDED="true" ID="ID_364842287" MODIFIED="1433399016464" POSITION="right" TEXT="autonomy">
+<node CREATED="1433398877141" ID="ID_1444241873" MODIFIED="1433398894793" TEXT="service boundary"/>
+<node CREATED="1433398922591" ID="ID_379044233" MODIFIED="1433398926554" TEXT="zone"/>
+<node CREATED="1433398895321" ID="ID_525554805" MODIFIED="1433398909334" TEXT="coarse-grained"/>
+<node CREATED="1433398909827" ID="ID_1006366857" MODIFIED="1433398946073" TEXT="inside zone intra-zone"/>
+<node CREATED="1433398946510" ID="ID_109380238" MODIFIED="1433398960186" TEXT="between zone inter-zone"/>
+<node CREATED="1433398978677" ID="ID_899597913" MODIFIED="1433398993199" TEXT="involve independently"/>
+<node CREATED="1433398993446" ID="ID_734991614" MODIFIED="1433398997542" TEXT="separate"/>
+<node CREATED="1433398998234" ID="ID_625533023" MODIFIED="1433399010405" TEXT="local decision"/>
+</node>
+<node CREATED="1433398871348" ID="ID_233005136" MODIFIED="1433399436008" POSITION="right" TEXT="Rules are for the obedience of fools and the guidance of wise men."/>
+</node>
+</map>
