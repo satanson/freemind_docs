a4524c04157622f9e15813a22558466e44cbde1d
satanson <ranpanf@gmail.com>
2017-10-25 09:24:01 +0800

    auto sync at 2017_10_25.09:24:01

--- llvm.mm
+++ llvm.mm
@@ -1,4 +1,15 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1508894246448" ID="ID_353687654" MODIFIED="1508894251308" TEXT="llvm"/>
+<node CREATED="1508894246448" ID="ID_353687654" MODIFIED="1508894251308" TEXT="llvm">
+<node CREATED="1508894304985" ID="ID_672619766" MODIFIED="1508894320238" POSITION="right" TEXT="IR: intermediate representation">
+<node CREATED="1508894328467" ID="ID_862582939" MODIFIED="1508894342794" TEXT="low-level language similar to asm"/>
+<node CREATED="1508894345249" ID="ID_1461888929" MODIFIED="1508894357023" TEXT="strongly typed RISC"/>
+<node CREATED="1508894362734" ID="ID_1758626475" MODIFIED="1508894416891" TEXT="SSA: static single assignment form"/>
+<node CREATED="1508894529182" ID="ID_1520414150" MODIFIED="1508894545755" TEXT="three isomorphic forms">
+<node CREATED="1508894546715" ID="ID_639832240" MODIFIED="1508894577997" TEXT="human-readable assembly format"/>
+<node CREATED="1508894555680" ID="ID_257064042" MODIFIED="1508894584269" TEXT="C++ object format for frontend"/>
+<node CREATED="1508894584560" ID="ID_997300936" MODIFIED="1508894595721" TEXT="dense bitcode format for serializing"/>
+</node>
+</node>
+</node>
 </map>
