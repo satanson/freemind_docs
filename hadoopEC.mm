240014a11024feb86b62dec387e43702ca06b02a
satanson <ranpanf@gmail.com>
2017-12-06 19:12:02 +0800

    auto sync at 2017_12_06.19:12:01

---
+++ hadoopEC.mm
@@ -0,0 +1,9 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1512558615932" ID="ID_1506305386" MODIFIED="1512558622455" TEXT="hadoopEC">
+<node CREATED="1512558650298" ID="ID_21072190" MODIFIED="1512558665255" POSITION="right" TEXT="StripedReconstructor">
+<node CREATED="1512558666831" ID="ID_1158183291" MODIFIED="1512558675031" TEXT="StripedBlockReconstructor"/>
+<node CREATED="1512558678379" ID="ID_1041862535" MODIFIED="1512558690960" TEXT="StripeBlockChecksumReconstructor"/>
+</node>
+</node>
+</map>
