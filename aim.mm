ecee57bc46edaf6868977f17375351ec32efb847
satanson <ranpanf@gmail.com>
2018-01-22 09:36:01 +0800

    auto sync at 2018_01_22.09:36:01

---
+++ aim.mm
@@ -0,0 +1,8 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1516584868345" ID="ID_758840770" MODIFIED="1516584886002" TEXT="aim">
+<node CREATED="1516584923865" ID="ID_271620256" MODIFIED="1516584929443" POSITION="right" TEXT="query"/>
+<node CREATED="1516584929692" ID="ID_1658166981" MODIFIED="1516584934660" POSITION="right" TEXT="Tx"/>
+<node CREATED="1516584936194" ID="ID_404058309" MODIFIED="1516584948039" POSITION="right" TEXT="consensus algorithm"/>
+</node>
+</map>
