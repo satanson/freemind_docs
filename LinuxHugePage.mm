37cae31126fb5eee234a9e5f3fbec18861e2dcf4
ranpanfeng <ranpanfeng@bytedance.com>
2019-04-11 11:42:02 +0800

    auto sync at 2019_04_11.11:42:02

---
+++ LinuxHugePage.mm
@@ -0,0 +1,13 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1554953796575" ID="ID_356583811" MODIFIED="1554953864111" STYLE="fork" TEXT="LinuxHugePage">
+<node CREATED="1554953866802" ID="ID_657096664" MODIFIED="1554953896862" POSITION="right" TEXT="Documentation/vm/hugetlbpage.txt">
+<node CREATED="1554953902043" ID="ID_652407474" MODIFIED="1554953932318" TEXT="kconfig">
+<node CREATED="1554953933241" ID="ID_358904288" MODIFIED="1554953956537" TEXT="CONFIG_HUGETLBFS"/>
+<node CREATED="1554953975305" ID="ID_1260320353" MODIFIED="1554953975984" TEXT="CONFIG_HUGETLB_PAGE">
+<node CREATED="1554953957022" ID="ID_512953870" MODIFIED="1554954007611" TEXT="selected automatically when CONFIG_HUGETLBFS is selected"/>
+</node>
+</node>
+</node>
+</node>
+</map>
