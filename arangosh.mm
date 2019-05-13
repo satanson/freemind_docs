a1d0f72178173f14f0fbf5c92fb4dbcefaca83f4
ranpanfeng <ranpanfeng@bytedance.com>
2019-05-13 10:48:01 +0800

    auto sync at 2019_05_13.10:48:01

---
+++ arangosh.mm
@@ -0,0 +1,8 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1557715484636" ID="ID_826963131" MODIFIED="1557715499512" TEXT="arangosh">
+<node CREATED="1557715524572" ID="ID_394464988" MODIFIED="1557715528271" POSITION="right" TEXT="collection"/>
+<node CREATED="1557715528681" ID="ID_450228597" MODIFIED="1557715529826" POSITION="right" TEXT="query"/>
+<node CREATED="1557715530053" ID="ID_887680903" MODIFIED="1557715530053" POSITION="right" TEXT=""/>
+</node>
+</map>
